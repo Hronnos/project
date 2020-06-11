ae7cdf2c5d3d7b171f3c2d0816edcfe875405465
Nick <nickizo@mail.ru>
2020-06-11 17:27:39 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+"use strict";
+const robot = {
+    _energyLevel: 100,
+    recharge(){
+      this._energyLevel += 30;
+      console.log(`Recharged! Energy is currently at ${this._energyLevel}%.`);
+    }
+  };
+  robot.recharge();
+
+//   const result = confirm('Are you');
+//   console.log(result);
+ 
+// const answer = +prompt("dfdf?", "");
+//  console.log(answer+ 5);
+
+const answers = [];
+
+answers[0] = prompt('What is your name?', '');
+answers[1] = prompt('What is your surname?', '');
+answers[2] = prompt('How old are yoy?', '');
+
+document.write(answers);
